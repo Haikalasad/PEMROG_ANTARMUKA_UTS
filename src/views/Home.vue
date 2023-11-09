c2abd3581b2aa811b62a70b33b1aa0ecbd7dee69
Haikalasad <muhammad.haikal.21@student.if.ittelkom-sby.ac.id>
2023-11-09 13:50:20 +0700

    Top sell fix 2

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -24,6 +24,5 @@ import herosection from "@/components/Hero-section.vue"
 import footerSection from "@/components/footer.vue"
 import TopSell from "@/components/TopSell.vue"
 import produk from "@/components/OurProducts.vue"
-import TopSell from "@/components/TopSell.vue"
 
 </script>
